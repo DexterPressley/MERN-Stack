b26b7f5cc9d20fc9ea7b18dfad8c4b302855ca8c
Daniel DuVall <duvall.daniel05@gmail.com>
2025-10-30 14:29:00 -0400

    LoggedInName.tsx now actually shows the user's name

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "react";
+
 function LoggedInName() {
+  
+  const [userName, setUserName] = useState('');
+
+  //get user data from local storage
+  useEffect(() => {
+    const userData = localStorage.getItem('user_data');
+    if (userData) {
+      try {
+        const user = JSON.parse(userData);
+        const fullName = `${user.firstName} ${user.lastName}`;
+        setUserName(fullName);
+      }
+      catch (error) {
+        console.error('Error parsing user data from localStorage', error);
+        setUserName('User');
+      }
+    } else {
+      setUserName('User');
+    }
+  }, []);
+
   function doLogout(event: any): void {
     event.preventDefault();
     localStorage.removeItem('user_data');
