2334f47e0b975314313797d2b86bceba828ef069
SaviortheCoder <161176820+SaviortheCoder@users.noreply.github.com>
2025-11-13 14:55:55 -0500

    Update App.tsx
    undid changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,19 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
-
-function AppContent() {
-  const location = useLocation();
-  
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/cards" element={<CardPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </>
-  );
-}
+import VerifyEmail from './pages/VerifyEmail';
 
 function App() {
   return (
     <BrowserRouter>
-      <AppContent />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cards" element={<CardPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+      </Routes>
     </BrowserRouter>
   );
 }
