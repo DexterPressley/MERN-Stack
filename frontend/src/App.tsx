cc82b7b3f0b61e0d5fde859174378b28d17d92e7
dessi20 <ndessi20@gmail.com>
2025-11-14 16:30:06 -0500

    Added food landing page with search/add/delete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,14 @@ import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import VerifyEmail from './pages/VerifyEmail';
+import FoodLanding from './pages/FoodLanding';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/cards" element={<CardPage />} />
+        <Route path="/cards" element={<FoodLanding />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
